946fa1f9ffc28dc0d544569c4ac792e548e99c38
jimmydin7 <turnspeaker@gmail.com>
2025-08-28 00:17:43 +0300

    add ysws catalog back tool

---
+++ tools/ysws_catalog.py
@@ -0,0 +1,15 @@
+# add your ysws to the ysws list at ysws.hackclub.com" where users will enter data and it will auto generate the yml code and open the ysws catalog to merge
+
+data_link = "https://github.com/hackclub/YSWS-Catalog/blob/main/data.yml"
+
+def generate_yml(name, description, website, slack, slackChannel, status, deadline):
+    
+    yml_snippet = f"""
+- name: {name}
+  description: {description}
+  website: {website}
+  slack: {slack}
+  slackChannel: {slackChannel}
+  status: {status}
+  deadline: {deadline}
+"""
